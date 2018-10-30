82a4509a58445216b9c386d6fce9f1badf601c62
tombshaw <43508967+tombshaw@users.noreply.github.com>
2018-10-29 22:01:01 -0700

    Update file to use MAVLINK header

--- Data/extractData.py
+++ Data/extractData.py
@@ -1,7 +1,6 @@
+
 """
 CS 50100 Group 16
-Written by Tom Shaw
-
 Python script to extract raw drone data from Mission Planner output
 
 1. input csv file name
@@ -15,6 +14,9 @@ import numpy as np
 import datetime
 import os
 
+from MavlinkParameters import mavlink_types
+from MavlinkParameters import mavlink_param
+from MavlinkParameters import mavlink_index
 
 #change this to name of file - MUST BE in XLS format to work with openpyxl!!
 name = "2018-10-07 13-49-59.csv"
@@ -38,123 +40,14 @@ if(cuttime):
     print("This will process file %s"%(name))
     print("The data will start at %s"%(startt))
     print("The data will end at %s"%(endt))
+    print("\n")
 
-
-mavlink_param = []
-mavlink_index = []
-mavlink_types = []
-
-#mavlink_ahrs_t
-mavlink_types.append("mavlink_ahrs_t")
-param = ["timestamp","omegaIx","omegaIy","omegaIz","accel_weight","renorm_val","error_rp","error_yaw"]
-index =  [1,12,14,16,18,20,22,24]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_ahrs2_t
-mavlink_types.append("mavlink_ahrs2_t")
-param = ["timestamp","roll","pitch","yaw","altitude","lat","lng"]
-index =  [1,12,14,16,18,20,22]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_ahrs3_t
-mavlink_types.append("mavlink_ahrs3_t")
-param = ["timestamp","roll","pitch","yaw","altitude","lat","lng"]
-index =  [1,12,14,16,18,20,22]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_attitude_t
-mavlink_types.append("mavlink_attitude_t")
-param = ["timestamp","time boot ms","roll angle","pitch angle","yaw angle","roll rate","pitch rate","yaw rate"]
-index =  [1,12,14,16,18,20,22,24]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#TS - battery data does not change
-#mavlink_battery_status_t
-#mavlink_types.append("mavlink_battery_status_t")
-#param = ["timestamp", "Current Consumed","Energy Consumed","Battery temperature","battery current","battery function","battery remaining","battery time remaining"]
-#index = [1,12,14,16,20,24,28,30]
-#mavlink_param.append(param)
-#mavlink_index.append(index)
-
-#mavlink_ekf_status_report_t
-mavlink_types.append("mavlink_ekf_status_report_t")
-param = ["timestamp","velocity_variance","pos_horiz_variance","pos_vert_variance","compass_variance","terrain_alt_variance","flags","airspeed_variance"]
-index = [1,12,14,16,18,20,22,24]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_global_position_int_t
-mavlink_types.append("mavlink_global_position_int_t")
-param = ["timestamp", "time_boot_ms","lat","lon","alt","relative_alt","vx","vy","vz","hdg"]
-index = [1,12,14,16,18,20,22,24,26,28]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_gps_raw_int_t
-mavlink_types.append("mavlink_gps_raw_int_t")
-param = ["timestamp", "time_usec","lat","lon","alt","eph","epv","vel","cog","fix_type","satelliets_visibile","alt_ellipsoid","h_acc","v_acc","vel_acc","hdg_acc"]
-index = [1,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_gps2_raw_t
-mavlink_types.append("mavlink_gps2_raw_t")
-param = ["timestamp", "time_usec","lat","lon","alt","dgps_age","eph","epv","vel","cog","fix_type","satelliets_visibile","dgps_numch"]
-index = [1,12,14,16,18,20,22,24,26,28,30,32,34]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_mission_current_t
-mavlink_types.append("mavlink_mission_current_t")
-param = ["timestamp", "seq"]
-index = [1,12]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_nav_controller_output_t
-mavlink_types.append("mavlink_nav_controller_output_t")
-param = ["timestamp", "nav_roll","nav_pitch","alt_error","aspd_error","xtrack_error","nav_bearing","target_bearing","wp_dist"]
-index = [1,12,14,16,18,20,22,24,26]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_raw_imu_t
-mavlink_types.append("mavlink_raw_imu_t")
-param = ["IMU timestamp","Xaccel","Yaccel","Zaccel","XGyro","YGyro","ZGyro","XMag","YMag","ZMag"]
-index = [1,14,16,18,20,22,24,26,28,30]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_servo_output_raw_t
-mavlink_types.append("mavlink_servo_output_raw_t")
-param = ["Servo timestamp","Servo utime","Servo 1","Servo 2","Servo 3","Servo 4"]
-index = [1,12,14,16,18,20]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_system_time_t
-mavlink_types.append("mavlink_system_time_t")
-param = ["timestamp","time_unix_usec","time_boot_ms"]
-index = [1,12,14]
-mavlink_param.append(param)
-mavlink_index.append(index)
-
-#mavlink_vibration_t
-mavlink_types.append("mavlink_vibration_t")
-param = ["timestamp","vibration_x","vibration_y","vibration_z"]
-index = [1,14,16,18]
-mavlink_param.append(param)
-mavlink_index.append(index)
 
 
 #error check
 for typ in range(len(mavlink_types)):
     if(len(mavlink_param[typ]) != len(mavlink_index[typ])):
-        print("ERROR: mavlink type %s has mismatched index and param size" %(mavlink_types[typ]))
+        raise ValueError("ERROR: mavlink type " + mavlink_types[typ] + " has mismatched index and param size")
 
 #Read data from file
         
@@ -228,4 +121,14 @@ if(appenddata):
                 file.close()
                     
     
-    
+#calculate statistics
+for typ in range(len(mavlink_types)):
+    firsttime = mavlink_data[typ][0][0]
+    firsttime = firsttime.replace("T"," ")
+    lasttime = mavlink_data[typ][0][-1]
+    lasttime = lasttime.replace("T"," ")
+    firstT = datetime.datetime.strptime(firsttime, '%Y-%m-%d %H:%M:%S.%f')
+    lastT = datetime.datetime.strptime(lasttime, '%Y-%m-%d %H:%M:%S.%f')
+    timediff = lastT - firstT
+    rate = len(mavlink_data[typ][0]) / (timediff.total_seconds())
+    print("Parameter %s has a rate of %f" % (mavlink_types[typ],rate))
