15f50600eacca979835309e7e6d55dc6e13d2e57
tombshaw <43508967+tombshaw@users.noreply.github.com>
2018-11-17 22:54:18 -0800

    Update LearnMulti.py
    update example

--- LearnMulti.py
+++ LearnMulti.py
@@ -9,8 +9,8 @@ This module calls the multi_param_learn function with multiple inputs
 
 from multi_param_learn import multi_param_learn
 
-paramlist = ['mavlink_raw_imu_t_XGyro','mavlink_raw_imu_t_YGyro']
-paramwight = [1.0,1.0]
+paramlist = ['mavlink_raw_imu_t_XGyro','mavlink_raw_imu_t_YGyro','mavlink_ahrs3_t_roll','mavlink_raw_imu_t_Zaccel','mavlink_raw_imu_t_XMag','mavlink_raw_imu_t_YMag','mavlink_vibration_t_vibration_z']
+paramweight = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 datapath = 'Data2/'
 
-multi_param_learn(paramlist,paramwight,datapath)
+multi_param_learn(paramlist,paramweight,datapath)
